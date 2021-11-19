7eb3f3c39da3fd3ae7e9587f3cc508386a323c54
SahilHathi <91531673+SahilHathi@users.noreply.github.com>
2021-11-19 03:17:11 +0000

    Finalising
    Finished project

--- ContactBook.py
+++ ContactBook.py
@@ -1,3 +1,6 @@
+#importing os to help work with files, ie: to create a csv file if it's missing
+import os
+
 # Creating a class to structure the data better
 class User:
     def __init__(self, firstn, lastn, age, phone_no, dob):
@@ -21,10 +24,31 @@ class User:
         return_string += "--------------------------------------"
         return return_string
 
-# List data structure creates an empty section of memory that can be edit stuff onto it
-
 contacts = list()
+
+# List data structure creates an empty section of memory that can be edit stuff onto it
+# If statement to see whether or not the file is in the system, if not it will create one
+
+# This is used to save the data from previous inputs. If the file isn't there then that means that the data has
+# been deleted, if it is there then it'll use what is already on the file.
+
+if os.path.isfile("contacts.csv"):
+    with open("contacts.csv") as f:
+
+# This will automatically read the lines
+
+        f_list = f.readlines()
+        for f_line in f_list:
+
+# The .rstrip removes the \n at the end and the .split will removes the commas.
+
+            contact_data = f_line.rstrip()
+            contact_data = f_line.split(",", ".")
+            contact = User(contact_data[0], contact_data[1], contact_data[2], contact_data[3], contact_data[4],)
+            contacts.append(contact)
+
 users_input = ""
+
 
 User_name = input("What is your name? ")
 
@@ -35,18 +59,20 @@ print("Welcome to " + User_name + "'s Contact Book")
 while users_input != "e":
     print("Please select one of these options:")
     print("1 - Input a contact")
-    print("2- Display contacts")
+    print("2 - Display contacts")
     print("3 - Search for a contact")
     print("e - Exit terminal")
     users_input = input("Select option: ")
 
 # Create a conditional check
+
     if users_input == "1":
         print("Please enter the contact's information")
 
 # This will be user input
 # There will be specific data types for each one, ages should be number integers as well as phonenumbers,
 # preventing any type of non-numerical inputs
+
         first_name = input("First Name = ")
         last_name = input("Last Name = ")
 
@@ -72,15 +98,19 @@ while users_input != "e":
         dob = input("Their date of birth = ")
 
         the_contact = User(first_name, last_name, age, phone_number, dob)
+
 # This stores the contact data
+  
         contacts.append(the_contact)
         print("Thank you")
     elif users_input == "2":
         for contact in contacts:
             print(contact)
         input("All contacts have now been displayed. Please press the Enter key to continue")
+
 # The .lower function allows the iser to input a capital 'e' "(E)" and the string method would still accept it as the
 # exit function. Just a small quality of life feature.
+
     elif users_input == "3":
         search = input("Enter a contact's name to search for\n")
         for contact in contacts:
@@ -92,9 +122,10 @@ while users_input != "e":
         break
 
 # If the file doesn't exist this should handle that gap of information
+
 with open("contacts.csv", "w") as f:
     for contact in contacts:
-        f.write(f'{contact.firstn}, {contact.lastn}, {contact.age}, {contact.phone_no}, {contact.dob}')
+        f.write(f'{contact.firstn}, {contact.lastn}, {contact.age}, {contact.phone_no}, {contact.dob}\n')
 
 
-print("Thank You")
+print("Thank you for using this application")
